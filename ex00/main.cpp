351fdef76456136b4a696b6e3fd0e4a914a57ad8
dnakano <dnakano@student.42tokyo.jp>
2021-01-12 11:37:02 +0900

    Ex00: Add: main.cpp (as subject)

---
+++ ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 11:24:02 by dnakano           #+#    #+#             */
+/*   Updated: 2021/01/12 11:25:21 by dnakano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "Fixed.hpp"
+
+int main(void) {
+  Fixed a;
+  Fixed b(a);
+  Fixed c;
+
+  c = b;
+  std::cout << a.getRawBits() << std::endl;
+  std::cout << b.getRawBits() << std::endl;
+  std::cout << c.getRawBits() << std::endl;
+  return 0;
+}
