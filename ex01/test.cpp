6363baa3f5c12515bc0ef8c97661a73d4dad1357
dnakano <dnakano@student.42tokyo.jp>
2021-01-13 18:15:33 +0900

    Ex01: Add: Another test

---
+++ ex01/test.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/13 18:02:25 by dnakano           #+#    #+#             */
+/*   Updated: 2021/01/13 18:15:04 by dnakano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "Fixed.hpp"
+
+int main(void)
+{
+  Fixed fixed;
+
+  fixed = Fixed((float)FIXED8_MAX);
+  std::cout << "fixed: " << fixed << std::endl;
+  fixed = Fixed((float)FIXED8_MIN);
+  std::cout << "fixed: " << fixed << std::endl;
+  fixed = Fixed((float)FIXED8_ABSMIN);
+  std::cout << "fixed: " << fixed << std::endl;
+  fixed = Fixed((float)(-FIXED8_ABSMIN));
+  std::cout << "fixed: " << fixed << std::endl;
+  fixed = Fixed((float)(FIXED8_ABSMIN - 0.00000001));
+  std::cout << "fixed: " << fixed << std::endl;
+  fixed = Fixed(0.0f);
+  std::cout << "fixed: " << fixed << std::endl;
+  fixed = Fixed(4242424.242f);
+  std::cout << "fixed: " << fixed << std::endl;
+  fixed = Fixed(-4242424.242f);
+  std::cout << "fixed: " << fixed << std::endl;
+}
